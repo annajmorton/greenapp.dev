add6e1f792a8cc98172f39964ff23b6b43095596
annajmorton <annajmorton@gmail.com>
2017-04-17 12:31:49 -0500

    corrected landing page text

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -235,7 +235,7 @@
                 <div class="post lg title"><a class="post lg title" href="#contactus"><h2>click here to contact us!</h2></a></div>
            </div>
 
-           <div class="textblk"><h4>Buildings are responsible for roughly 30% of green-house gas emissions in the US. There is movement in the construction industry towards green design, but these efforts are almost exclusively based on modeled energy savings. There is a knowledge gap between this modeled savings and actual energy consumption.</h4></div>
+           <div class="textblk"><h4>Buildings are responsible for roughly 40% of greenhouse gas emissions in the US. There is major movement in the construction industry towards energy efficiency, but these efforts are largely based on modeled energy savings or prescriptive measures. There is a knowledge gap between these designed efficiencies and actual energy consumption in implementation.</h4></div>
            <div class="section">
                 <div class="post xs"><img class="sketch" src="images/designer.png"></div>
                 <div class="post sm"><img class="sketch" src="images/builder.png"></div>
@@ -243,13 +243,13 @@
                 <div class="post xs"><img class="sketch" src="images/thegap/gap.png"></div>
                 <div class="post sm"><img class="sketch" src="images/thegap/user_gap.png"></div>
            </div>
-           <div class="textblk"><h4>Talking walls will close this gap by helping project teams see if energy saving goals measure up. It provides designers and builders with feedback on designs strategies, improving future projects. It helps owners recoup the energy savings of efficiency investments. It promotes efficiency awareness to building occupants and optimizes operation.</h4></div>
+           <div class="textblk"><h4>Talking walls closes this gap by helping project teams see if energy saving goals measure up. It helps designers and builders improve future projects through feedback, helps operators and owners recoup efficiency investments, and influences user behavior. </h4></div>
            <div class="section">
                 <div class="post lg"><img class="sketch" src="images/theapp.png"></div>
            </div>
-           <div class="textblk"><h4>We will be launching the first protoype summer 2017. This phase includes uploading design information and utility data to create a comparison feedback loop. </h4></div>
+           <div class="textblk"><h4>We will be launching the first prototype Summer 2017. This phase includes uploading design information and utility data to create a comparison feedback loop. </h4></div>
            <div class="section">
-                <div class="post lg title"><h2>1 upload design energy model</h2></div>
+                <div class="post lg title"><h2>1 upload energy goals</h2></div>
                 <div class="post sm"><img class="sketch" src="images/phase1/designer_p1.png"></div>
                 <div class="post xs"><img class="sketch" src="images/phase1/arrow_p1.png"></div>
                 <div class="post sm"><img class="sketch" src="images/phase1/app_p1a.png"></div>
