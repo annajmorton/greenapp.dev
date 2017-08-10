537f4e5fff1d3c048d960ee7db37ddcbb8bd55d7
annajmorton <annajmorton@gmail.com>
2017-08-10 11:10:57 -0700

    modified routes removing access to the ibpsa excercise

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -11,15 +11,15 @@
 |
 */
 
-// Route::get('visitor', 'VisitorController@showhome')->name('visitor');
+Route::get('visitor', 'VisitorController@showhome')->name('visitor');
 
-// Route::get('', function () {
-//     return redirect()->route('visitor');
-// });
-// Route::post('visitor','VisitorController@store');
-// Route::get('alpha','VisitorController@store');
-// Route::post('eplus', 'EplusController@uploadFiles');
-// Route::put('eplus', 'EplusController@runDefault');
+Route::get('', function () {
+    return redirect()->route('visitor');
+});
+Route::post('visitor','VisitorController@store');
+Route::get('alpha','VisitorController@store');
+Route::post('eplus', 'EplusController@uploadFiles');
+Route::put('eplus', 'EplusController@runDefault');
 
 // // testing site visiting capacity
 // Route::get('loaderio-575b745215eb9a611a928399a24abac8', function(){
@@ -30,17 +30,20 @@
 
 
 // these are all for the Ibpsa conference
-Route::resource('ibpsa17', 'Ibpsa17Controller');
-Route::get('/guess', function () {
-    return view('ibpsa/guess');
-});
-Route::get('/score', function () {
-    return view('ibpsa/score');
-});
-Route::get('/post', function () {
-    return view('ibpsa/post');
-});
+// Route::resource('ibpsa17', 'Ibpsa17Controller');
+// Route::get('/guess', function () {
+//     return view('ibpsa/guess');
+// });
+// Route::get('/score', function () {
+//     return view('ibpsa/score');
+// });
+// Route::get('/post', function () {
+//     return view('ibpsa/post');
+// });
 
-Route::get('', function () {
-    return view('ibpsa/guess');
-});
+// Route::get('', function () {
+//     return view('ibpsa/guess');
+// });
+// Route::get('*', function () {
+//     return view('ibpsa/guess');
+// });
